5b1d0b9b0f8e851e86a8c6b503ab3b5dfe064ec8
Furqan Aziz <furqan@invozone.com>
2020-08-10 19:45:04 +0500

    backend separate folder

--- api/services/firestore.js
+++ api/services/firestore.js
@@ -1,4 +1,4 @@
-const admin = require('firebase-admin');
+const admin = require('./node_modules/firebase-admin');
 const serviceAccount = require('../constants/parkinglot-key.json');
 let db;
 try {
